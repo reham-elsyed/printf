eaff9682417116de5476691bea703373d9268a2b
1stjanjona <jannat.hammouche@outlook.com>
2023-09-17 15:43:59 -0700

    edited _printf

--- _printf.c
+++ _printf.c
@@ -11,8 +11,8 @@ int _printf(const char *format, ...)
 va_list args;
 va_start(args, format);
 int prntd_ch = 0;
-int n, printed= 0;
-int flags, wdth, perc, size, buff_int; = 0;
+int n, printed = 0;
+int flags, wdth, perc, size, buff_int = 0;
 char buff[BUFF_SIZE];
 
 if (format == NULL)
