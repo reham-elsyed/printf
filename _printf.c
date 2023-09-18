ffc2af7491a62e0fec85a38688212f3c98031d1e
1stjanjona <jannat.hammouche@outlook.com>
2023-09-18 10:52:18 -0700

    again & again

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int prntd_ch = 0, prntd_str = 0, n = 0;
+	int prntd_ch = 0, prntd_str = 0, prntd_n = 0, n = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -17,34 +17,39 @@ int _printf(const char *format, ...)
 	{
 		if (format[n] == '%')
 		{
+			format++;
+			if (format[n + 1] == 'c')
+			{
+				our_putchar(va_arg(args, int));
+				prntd_ch++;
+			}
+			else if (format[n + 1] == 's')
+			{
+				prntd_str = our_puts(va_arg(args, char *));
+				prntd_ch += (prntd_str - 1);
+			}
+			else if (format[n + 1] == '%')
+			{
+				our_putchar('%');
+				prntd_ch++;
+			}
+			else if (format[n + 1] == 'd' || format[n + 1] == 'i')
+			{
+				prntd_n = our_putchar(va_arg(args, int));
+				prntd_ch += prntd_n;
+			}
+			else
+			{
+				our_putchar(format[n]);
+				prntd_ch++;
+			}
+		}
+		else
+		{
 			our_putchar(format[n]);
+			prntd_ch++;
 		}
-		else if (format[n + 1] == 'c')
-		{
-			our_putchar(va_arg(args, int));
-			n++;
-		}
-		else if (format[n + 1] == 's')
-		{
-			prntd_str = our_puts(va_arg(args, char *));
-			n++, prntd_ch += (prntd_str - 1);
-		}
-		else if (format[n + 1] == '%')
-		{
-			our_putchar('%');
-			n++;
-		}
-		else if (format[n + 1] == 'd')
-		{
-			our_putchar(va_arg(args, int));
-			n++;
-		}
-		else if (format[n + 1] == 'i')
-		{
-			our_putchar(va_arg(args, int));
-			n++;
-		}
-		n++, prntd_ch += 1;
+		format++, prntd_ch += 1;
 	}
 	va_end(args);
 	return (prntd_ch);
