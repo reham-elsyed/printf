138e424d9944da647f561f854a4ed4360b005430
reham-elsyed <rehamshipl666@gmail.com>
2023-09-18 15:42:50 -0700

    update printf , printpercent

--- _printf.c
+++ _printf.c
@@ -8,46 +8,46 @@
  */
 int _printf(const char *format, ...)
 {
-	int prntd_ch = 0, prntd_str = 0, prntd_n = 0, prntd_n_str = 0, n = 0;
-	va_list args;
+int prntd_ch = 0, prntd_str = 0, prntd_n = 0, prntd_n_str = 0, n = 0;
+va_list args;
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (format && format[n] != '\0')
-	{
-		if (!format || (format[0] == '%' && !format[1]))
-			return (-1);
-		if (format[n] == '%')
-		{
-			n++;
-			if (format[n] == 'c')
-			 prntd_ch += print_char(args);
-			else if (format[n] == 's')
-    prntd_str += print_string(args);
-			else if (format[n] == '%')
-			{
-				our_putchar('%');
-				prntd_ch++;
-			}
-			else if (format[n + 1] == 'd' || format[n + 1] == 'i')
-			{
-				prntd_n = our_putchar(va_arg(args, int));
-				prntd_n_str = our_puts(va_arg(args, char *));
-				prntd_ch += (prntd_n + (prntd_n_str - 1));
-			}
-			else
-			{
-				our_putchar(format[n]);
-				prntd_ch += 26;
-			}
-		}
-		else
-		{
-			our_putchar(format[n]);
-			prntd_ch++;
-		}
-		n++;
-	}
-	va_end(args);
-	return (prntd_ch);
+while (format && format[n] != '\0')
+{
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[n] == '%')
+{
+n++;
+if (format[n] == 'c')
+prntd_ch += print_char(args);
+else if (format[n] == 's')
+prntd_str += print_string(args);
+else if (format[n] == '%')
+{
+our_putchar('%');
+prntd_ch++;
+}
+else if (format[n + 1] == 'd' || format[n + 1] == 'i')
+{
+prntd_n = our_putchar(va_arg(args, int));
+prntd_n_str = our_puts(va_arg(args, char *));
+prntd_ch += (prntd_n + (prntd_n_str - 1));
+}
+else
+{
+our_putchar(format[n]);
+prntd_ch += 26;
+}
+}
+else
+{
+our_putchar(format[n]);
+prntd_ch++;
+}
+n++;
+}
+va_end(args);
+return (prntd_ch);
 }
