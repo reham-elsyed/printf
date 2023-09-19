d29cf7abcd7c4dbe0f3e0eb405dde13d40f3561f
1stjanjona <jannat.hammouche@outlook.com>
2023-09-19 03:18:13 -0700

    int i

--- _print_int_i.c
+++ _print_int_i.c
@@ -7,8 +7,34 @@
  */
 int print_int_i(va_list args)
 {
-	int i = va_arg(args, int);
+	int ntgr = va_arg(args, int);
+	int prntd_ntgr = 1, n, m = ntgr % 10;
+	int num, pwr = 1;
 
-	our_putchar(i);
-	return (1);
+	n = ntgr, ntgr = ntgr / 10;
+
+	if (n > 0)
+	{
+		while ((n / 10) != 0)
+		{
+			pwr = pwr * 10, n = n / 10;
+		}
+		n = ntgr;
+		while (pwr > 0)
+		{
+			num = ntgr / pwr;
+			our_putchar(num + '0');
+			n = n - (num * pwr);
+			pwr = pwr / 10;
+			prntd_ntgr++;
+		}
+	}
+	if (m < 0)
+	{
+		our_putchar('-');
+		n = -n, ntgr = -ntgr, m = -m;
+		prntd_ntgr++;
+	}
+	our_putchar(m + '0');
+	return (prntd_ntgr);
 }
