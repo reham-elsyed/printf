94d5b576ec6bf3b847f73892ab47a29efd9a82fe
1stjanjona <jannat.hammouche@outlook.com>
2023-09-19 15:01:12 -0700

    trial again

--- _printf.c
+++ _printf.c
@@ -7,44 +7,56 @@
  */
 int _printf(const char *format, ...)
 {
-	int prntd_ch = 0, prntd_str = 0, prntd_n = 0, orntd_n_str = 0, n = 0;
 	va_list args;
+	int prntd_chs = 0, nmbr;
+	char c, *str;
+	unsigned int num;
 
 	va_start(args, format);
-	while (format && format[n] != '\0')
+	while (*format)
 	{
-		if (!format || (format[0] == '%' && !format[1]))
-			return (-1);
-		if (format[n] == '%')
+		if (*format != '%')
 		{
-			n++;
+			our_putchar(*format), prntd_chs++;
+		}
+		else
+		{
+			format++;
 			if (format[n] == 'c')
-				prntd_ch += print_char(args);
-			else if (format[n] == 's')
-				prntd_str += print_string(args);
-			else if (format[n] == '%')
 			{
-				our_putchar('%');
-				prntd_ch++;
+				c = va_arg(args, int), our_putchar(c), prntd_chs++;
 			}
-			else if (format[n + 1] == 'd' || format[n + 1] == 'i')
+			else if (format[n] == 's')
 			{
-				prntd_n = our_putchar(va_arg(args, int));
-				prntd_n_str = our_puts(va_arg(args, char *));
-				prntd_ch += (prntd_n + (prntd_n_str - 1));
+				*str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				while (*str)
+				{
+					our_putchar(*str), str++, prntd_chs++;
+				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				nmbr = va_arg(args, int), prntd_chs += print_number(nmbr);
+			}
+			else if (*format == 'u')
+			{
+				num = va_arg(args, unsigned int), prntd_chs += print_unsigned(num);
 			}
 			else
 			{
-				our_putchar(format[n]);
-				prntd_ch += 26;
+				our_putchar(*format), prntd_chs += 26;
 			}
 		}
 		else
 		{
-			our_putchar(format[n]), prntd_ch++;
+			our_putchar(*format), prntd_chs++;
 		}
-		n++;
+		format++;
 	}
 	va_end(args);
-	return (prntd_ch);
+	return (prntd_chs);
 }
