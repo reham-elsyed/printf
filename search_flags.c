e9cd4787edac9d0ee4b7e7e4743c810c234b2df0
1stjanjona <jannat.hammouche@outlook.com>
2023-09-20 08:56:15 -0700

    now

---
+++ search_flags.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * search_flags - check the format for _printf
+ * @format: format ('c', 's', 'i', 'd'...etc)
+ * Return: failed return (NULL), on success return (format)
+*/
+int (*search_flags(const char *format))(va_list)
+{
+        unsigned int count = 0;
+        specifier_search flags_f[] = {
+                {"c", print_char}, {"s", print_string}, {"%", print_percent},
+                {"i", print_int_i}, {"d", print_int_d}, {"b", printf_binary},
+                {"u", print_uint},
+                {NULL, NULL}
+        };
+        while (flags_f[count].srch)
+                if (flags_f[count].srch[0] == (*format))
+                        return (flags_f[count].f);
+                count++;
+        return (NULL);
+}
