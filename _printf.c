6ed81e799f97e786f6d59259903e6306868c1284
reham-elsyed <rehamshipl666@gmail.com>
2023-09-19 17:58:34 -0700

    update printf function

--- _printf.c
+++ _printf.c
@@ -7,56 +7,42 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int prntd_chs = 0, nmbr;
-	char c, *str;
-	unsigned int num;
+int char_count = 0;
+va_list args;
+va_start(args, format);
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			our_putchar(*format), prntd_chs++;
-		}
-		else
-		{
-			format++;
-			if (format[n] == 'c')
-			{
-				c = va_arg(args, int), our_putchar(c), prntd_chs++;
-			}
-			else if (format[n] == 's')
-			{
-				*str = va_arg(args, char *);
-				if (str == NULL)
-				{
-					str = "(null)";
-				}
-				while (*str)
-				{
-					our_putchar(*str), str++, prntd_chs++;
-				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				nmbr = va_arg(args, int), prntd_chs += print_number(nmbr);
-			}
-			else if (*format == 'u')
-			{
-				num = va_arg(args, unsigned int), prntd_chs += print_unsigned(num);
-			}
-			else
-			{
-				our_putchar(*format), prntd_chs += 26;
-			}
-		}
-		else
-		{
-			our_putchar(*format), prntd_chs++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (prntd_chs);
+if (!format || (format[0] == '\0' && !format[1]))
+	return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+	return (-1);
+while (*format)
+{
+if (format[0] == '%' && format[1] != '\0')
+{
+if (format[1] == 's' || format[1] == 'c' ||
+format[1] == 'd' || format[1] == 'x' || format[1] == '%')
+{
+handle_specifier(format[1], args);
+format++;
+}
+else if (format[1] == 'X' || format[1] == 'o' ||
+format[1] == 'i' || format[1] == 'u' || format[1] == 'p')
+{
+handle_int_specifiers(format[1], args);
+format++;
+}
+else
+{
+_putchar(format[0]);
+}
+}
+else
+{
+_putchar(format[0]);
+}
+format++;
+char_count++;
+}
+va_end(args);
+return (char_count);
 }
